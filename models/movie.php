fd439d96a744375ea941992289f05754e7c46e7f
TALEX1995 <tanase.alex@libero.it>
2023-07-21 15:17:33 +0200

    Added construct

--- models/movie.php
+++ models/movie.php
@@ -7,4 +7,14 @@ class Movie
     public $type;
     public $year_of_release;
     public $vote;
+
+    // Construct
+    function __construct(int $id, $title, $type, int $year_of_release, int $vote)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->type = $type;
+        $this->year_of_release = $year_of_release;
+        $this->vote = $vote;
+    }
 }
