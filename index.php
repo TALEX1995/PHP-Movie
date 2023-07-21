6d57a84f1bbd9ec124692636ecdd20696716cbec
TALEX1995 <tanase.alex@libero.it>
2023-07-21 15:01:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once __DIR__ . '/models/movie.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
